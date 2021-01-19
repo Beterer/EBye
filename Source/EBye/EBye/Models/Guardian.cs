e69669334450cd3fe60a518b24528d0a83d9ca4d
Florin Pilca <Florin.Pilca@gmail.com>
2021-01-19 21:05:20 +0200

    Basic guardians page

---
+++ Source/EBye/EBye/Models/Guardian.cs
@@ -0,0 +1,15 @@
+﻿namespace EBye.Models
+{
+    public class Guardian
+    {
+        public int GuardianId { get; set; }
+
+        public string FullName { get; set; }
+
+        public string Email { get; set; }
+
+        public string Phone { get; set; }
+
+        public bool ConfirmedDeath { get; set; }
+    }
+}
